c18bbfa1a884396357b1b0227b705728df4a9312
isSugar <1187584957@qq.com>
2021-12-12 22:02:55 +0800

    1.0.0版本

---
+++ app.js
@@ -0,0 +1,11 @@
+const express  = require(express)
+
+const app = express()
+
+app.get('/',(Request,Response) => {
+  Response.send('Hello Express!')
+})
+
+app.listen(8000,() => {
+  console.log("http://localhost:8000")
+})
